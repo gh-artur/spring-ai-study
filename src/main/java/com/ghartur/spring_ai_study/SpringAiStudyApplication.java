dabf80378e953b9fae3eb34cdfae36bd0f1cfde2
ArturSantos <arturfrancisco86@gmail.com>
2026-06-23 11:17:17 +0100

    init repo

---
+++ src/main/java/com/ghartur/spring_ai_study/SpringAiStudyApplication.java
@@ -0,0 +1,13 @@
+package com.ghartur.spring_ai_study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiStudyApplication.class, args);
+	}
+
+}
